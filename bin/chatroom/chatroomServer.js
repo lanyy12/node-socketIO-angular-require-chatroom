da38a03c22197b341f42c64d9711ed81d647d5cf
towersxu <987427795@qq.com>
2015-06-24 10:02:28 +0800

    i

--- bin/chatroom/chatroomServer.js
+++ bin/chatroom/chatroomServer.js
@@ -2,18 +2,30 @@
  * Created by taox on 15-5-25.
  */
 
-var socketIO = require('socket.io');
+var socketIO = require('socket.io'),
+  roomNamespaceArr = [];
 exports.initChatRoom = function (roomArr, server) {
   var io = socketIO(server);
   for (var i = 0; i < roomArr.length; i++) {
     var roomNamespace = io.of("/"+roomArr[i].id);
     roomConnected(roomNamespace);
+    roomNamespaceArr.push(roomNamespace);
   }
 };
 function roomConnected(roomNamespace){
   console.log("init namespace"+roomNamespace);
   roomNamespace.on("connection",function(socket){
-    console.log("a user connected");
-    console.log(socket.id);
+    //console.log(this.sockets.length);
+    //console.log(socket.id);
+    //console.log("==============================>>");
+    socket.on("room chat", function (msg) {
+      roomNamespace.emit('room chat',msg);
+    });
+    socket.emit('user number',{userNum:roomNamespace.onlineUser});
   });
+  roomNamespace.onlineUser = roomNamespace.sockets.length;
+  setInterval(function(){
+    roomNamespace.onlineUser = roomNamespace.sockets.length;
+  },10000);
 }
+
--- bin/dbconnector.js
+++ bin/dbconnector.js
@@ -20,16 +20,17 @@ exports.connect =  function(server){
   //    throw  err;
   //  }
     //使用自定义房间方式。
-    chatserver.setChatRoom([{"name":"杨过","id":"1","onlineUser":"100"},
-      {"name":"小龙女","id":"2","onlineUser":"100"},
-      {"name":"独孤求败","id":"3","onlineUser":"100"},
-      {"name":"东方不败","id":"4","onlineUser":"100"},
-      {"name":"令狐聪","id":"5","onlineUser":"100"},
-      {"name":"韦小宝","id":"6","onlineUser":"100"},
-      {"name":"雄霸天下","id":"7","onlineUser":"100"},
-      {"name":"风云","id":"8","onlineUser":"100"}],server);
+  var roomArr = [{"name":"杨过","id":"1","onlineUser":"100"},
+    {"name":"小龙女","id":"2","onlineUser":"100"},
+    {"name":"独孤求败","id":"3","onlineUser":"100"},
+    {"name":"东方不败","id":"4","onlineUser":"100"},
+    {"name":"令狐聪","id":"5","onlineUser":"100"},
+    {"name":"韦小宝","id":"6","onlineUser":"100"},
+    {"name":"雄霸天下","id":"7","onlineUser":"100"},
+    {"name":"风云","id":"8","onlineUser":"100"}];
+    //chatserver.setChatRoom(roomArr,server);
     //使用房间和namespace对应方式实现聊天。
-    //chatroomServer.initChatRoom(rows,server);
+    chatroomServer.initChatRoom(roomArr,server);
   //});
   //
   //connection.end();
